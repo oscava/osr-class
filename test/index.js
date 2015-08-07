d25a603e4468a6bf8a5fef230c9c36fd83e36553
张嘉华 <admin@cavacn.com>
2015-08-07 10:47:03 +0800

    Add constructor '$'
    Add constructor '$'

--- test/index.js
+++ test/index.js
@@ -1,11 +1,20 @@
 var Class = require("../");
 
+var EventEmitter = require("events").EventEmitter;
+
 var Person = Class.extends({
 	name:"person",
 	sex:1,
 	age:28,
+	$:function(inobj,pm2){
+		for(var key in inobj){
+			this[key] = inobj[key];
+		}
+		this.pm2 = pm2
+	},
 	sayHello:function(msg){
 		console.log("Hello , My name is %s, I'm %s years old, I'm %s, %s",this.name,this.age,this.sex == 0? "girl" : "boy" ,msg);
+		console.log("--->",this.pm2)
 	}
 });
 
@@ -36,14 +45,63 @@ var Girl = Person.extends({
 var Tom = new Boy({
 	name:"Tom",
 	age:16
-})
+},"play")
 
 Tom.sayHello("I like playing football");
 
 var Marry = new Girl({
 	name:"Marry",
 	age:14
-})
+},"sing")
 
 Marry.sayHello("I like singing");
+
+var Emitter = Class.extends(EventEmitter);
+
+var CallMe = Emitter.extends({
+	ring:function(from){
+		this.emit("ring",from);
+	},
+	handUp:function(){
+		this.isAnswer = false;
+		this.emit("handup");
+	},
+	answer:function(msg){
+		this.emit("answer",msg);
+	}
+})
+
+var callme = new CallMe();
+callme.on("ring",function(from){
+	this.from = from;
+	if(from=="Tom"){
+		this.handUp();
+	}else{
+		this.isAnswer = true;
+		console.log("me->%s:Oh , Hello, %s",from,from);
+	}
+});
+
+callme.on("handup",function(){
+	console.log("me:Handup,Keep doing my thing.");
+})
+
+callme.on("answer",function(msg){
+	if(!this.isAnswer){
+		console.log("me->%s:Already hand up ~",this.from);
+		return;
+	}
+	if(msg=="Borrow money"){
+		this.handUp();
+	}else{
+		console.log("%s->me:%s",this.from,msg);
+		console.log("me->%s: Hello,%s",this.from,msg);
+	}
+})
+
+callme.ring("Tom");
+callme.answer("How are u !");
+callme.ring("Marry");
+callme.answer("How are u !");
+callme.answer("Borrow money");
 
